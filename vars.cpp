51634527aa39a30cf78cdaa0a1e6837d3ab44352
Yc3t <ycetabi@gmail.com>
2024-02-11 14:47:57 +0100

    1st commit

---
+++ vars.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+    int num,num2;
+
+    cout<<"enter your lucky number\n";
+    cin>> num>>num2; 
+
+    cout<<"your new lucky number is "<< num+12+num2;
+
+
+
+
+
+
+
+    return 0;
+}
